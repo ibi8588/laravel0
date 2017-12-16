1f5b46edb7c7f6b92ef16a977f105b19fac25c3f
Ibrahim <ialdridge@lqdigital.com>
2017-12-15 16:19:00 -0800

    changed the show page so that users can click on the item to edit it.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-     <h1 style="text-align: center">{{$post->title}}</h1>
-    <small>Written on {{$post->created_at}}</small>
+  <div style="text-align: center">
+     <h1 class="display-1"><a href="{{$post->id}}/edit">{{$post->title}}</a></h1>
       <p>{{$post->body}}</p>
+    <small>Written on {{$post->created_at}}</small>
 
-
+  </div>
 @endsection
