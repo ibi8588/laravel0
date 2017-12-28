0be6a8a138c68b8c1b0494061dfc001c922288d0
Ibrahim <ialdridge@lqdigital.com>
2017-12-27 16:50:22 -0800

    changed view for show to add delete button

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,6 +5,9 @@
      <h1 class="display-1"><a href="{{$post->id}}/edit">{{$post->title}}</a></h1>
       <p>{{$post->body}}</p>
     <small>Written on {{$post->created_at}}</small>
-
+    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+      {{Form::hidden('_method', 'DELETE')}}
+      {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!!Form::close()!!}
   </div>
 @endsection
